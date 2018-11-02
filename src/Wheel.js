d3e229cf85f87ec6bd2595a0773eed8a571a69cf
Howlin Bash <howlinbash@posteo.de>
2018-11-02 22:05:25 +0000

    Replace duplicated code with an array

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { pat, question } from './faces';
 import './Wheel.css';
 
-const Wheel = ({ isSpinning }) => (
-  <div className="wheel">
-    <div className="window a">
-      <img src={isSpinning ? pat : question} />
-    </div>
-    <div className="window b">
-      <img src={isSpinning ? pat : question} />
-    </div>
-    <div className="window c">
-      <img src={isSpinning ? pat : question} />
-    </div>
-  </div>
-);
+class Wheel extends Component {
+  constructor(props) {
+    super(props);
+    this.windows = [...Array(3)]
+  }
+
+  render() {
+    const { isSpinning } = this.props;
+
+    return (
+      <div className="wheel">
+        {this.windows.map(window => (
+          <div className="window">
+            <img src={isSpinning ? pat : question} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default Wheel;
