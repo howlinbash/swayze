5ef21fc1f89ce6a351c8291019dfdb8e193ebe38
Howlin Bash <howlinbash@posteo.de>
2018-11-04 22:55:12 +0000

    Refactor Display as class

--- src/Display.js
+++ src/Display.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Display.css';
 
-const Display = ({ round }) => (
-  <div className="display">
-    <div className="message">
-      {round ? `ROUND ${round}` : 'GAME OVER'}
-    </div>
-  </div>
-);
+class Display extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: 'SPIN THE WHEEL' }
+  }
+
+  render() {
+    return (
+      <div className="display">
+        <div className="message">
+          {this.state.message}
+        </div>
+      </div>
+    );
+  };
+};
 
 export default Display;
