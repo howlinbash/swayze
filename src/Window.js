2b2de44a53df7a60543e0e4635fd797be04097f4
Howlin Bash <howlinbash@posteo.de>
2018-11-04 16:22:55 +0000

    Add onClick handler to Window

--- src/Window.js
+++ src/Window.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Face from './Face';
 import { question } from './faces';
 import './Window.css';
 
-const Window = ({ focus, id, isSpinning }) => (
-  <div className="window" key={id}>
-    {isSpinning ? (
-      focus === id ? <Face id={id} /> : null
-    ) : (
-      <img src={question} />
-    )}
-  </div>
-)
+class Window extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id) {
+    console.log('clicky', id);
+  }
+
+  render() {
+  const { focus, id, isSpinning } = this.props;
+
+  return (
+    <div
+      className="window"
+      key={id}
+      onClick={() => this.handleClick(id)}
+    >
+      {isSpinning ? (
+        focus === id ? <Face id={id} /> : null
+      ) : (
+        <img src={question} />
+      )}
+    </div>
+    )
+  }
+}
 
 export default Window;
