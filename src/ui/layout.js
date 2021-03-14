6b2210e6fec801a4da197cc9d1942894e4eb5314
Howlin Bash <howlinbash@posteo.de>
2021-03-15 01:30:25 +0200

    Strip UI of old props

--- src/ui/layout.js
+++ src/ui/layout.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { spin } from "../events";
 import { Frame } from "../design";
 import Marquee from "./marquee";
 import DisplayBoard from "./display-board";
 import ReelUnit from "./reel-unit";
-import { getLevel } from "../selectors";
 
 const Wallpaper = styled.div`
   display: grid;
@@ -15,30 +13,16 @@ const Wallpaper = styled.div`
   background-color: #282c34;
 `;
 
-const Layout = () => {
-  const level = useSelector(getLevel);
-  return (
-    <Wallpaper>
-      <Frame color="green" gtr="1fr 2fr" h="65vmin" pd="1.5vmin" w="80vmin">
-        <Marquee onClick={spin()} />
-        <Frame border="darkgrey" color="purple" gtr="auto 1fr" pd="1.5vmin">
-          <DisplayBoard
-            answer={null}
-            attempt={0}
-            gameOver={true}
-            isSpinning={false}
-            jeff={false}
-            round={0}
-          />
-          <ReelUnit
-            answer={null}
-            requestStartSpin={false}
-            faces={level}
-          />
-        </Frame>
+const Layout = () => (
+  <Wallpaper>
+    <Frame color="green" gtr="1fr 2fr" h="65vmin" pd="1.5vmin" w="80vmin">
+      <Marquee onClick={spin()} />
+      <Frame border="darkgrey" color="purple" gtr="auto 1fr" pd="1.5vmin">
+        <DisplayBoard />
+        <ReelUnit />
       </Frame>
-    </Wallpaper>
-  );
-}
+    </Frame>
+  </Wallpaper>
+);
 
 export default Layout;
--- src/ui/reel-unit.js
+++ src/ui/reel-unit.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Frame } from "../design";
 import Reel from "./reel";
-import { getIsSpinning } from "../selectors";
+import { getIsSpinning, getLevel } from "../selectors";
 
-const ReelUnit = ({ answer, requestStartSpin, faces }) => {
+const ReelUnit = () => {
   const [focus, setFocus] = useState(0);
   const [timerId, setTimerId] = useState("");
   const isSpinning = useSelector(getIsSpinning);
+  const level = useSelector(getLevel);
   const keys = ["alpha", "beta", "charlie"];
 
   useEffect(() => {
@@ -23,9 +24,8 @@ const ReelUnit = ({ answer, requestStartSpin, faces }) => {
 
   return (
     <Frame border="gold" color="brown" gtc="1fr 1fr 1fr" pd="1.5vmin">
-      {faces.map((face, index) => (
+      {level.map((face, index) => (
         <Reel
-          answer={answer}
           face={face}
           focus={focus}
           id={index}
