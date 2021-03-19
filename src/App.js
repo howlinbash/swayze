02bedd93e6a211aa8ee3775b86247af150e0773e
Howlin Bash <howlinbash@posteo.de>
2021-03-19 14:14:48 +0200

    Fix font rendering on safari

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 import { Layout } from "./ui";
+import "./font.css";
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Impact';
-    font-style: normal;
-    font-weight: normal;
-    src:
-      local('Impact'),
-      local('Impact'),
-      url('./fonts/impact.ttf')
-      format('truetype');
-  }
-
   body {
     margin: 0;
     padding: 0;
